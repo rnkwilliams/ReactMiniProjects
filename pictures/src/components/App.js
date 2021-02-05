b88bc0306160d37bf46ab3c7a211982a43c6c05f
rnkwilliams <rnkwilliams07@gmail.com>
2021-02-05 09:21:56 -0600

    create app component with imported search bar component including form input, onchange, and onFormSubmit

--- pictures/src/components/App.js
+++ pictures/src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import SearchBar from './SearchBar'
 
 const App = () => {
 
   return (
-    App
+    <div className="ui container" style={{marginTop: '10px'}}>
+      <SearchBar />
+    </div>
   )
 }
+
+export default App;
