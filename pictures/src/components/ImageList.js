e48df3077aa9570f162ef368807dafa9d9b1702b
rnkwilliams <rnkwilliams07@gmail.com>
2021-02-06 15:08:24 -0600

    create image list css component, import to image list component

--- pictures/src/components/ImageList.js
+++ pictures/src/components/ImageList.js
@@ -1,13 +1,15 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
 
-  const images = props.images.map(({description, id, urls}) => {
-    return <img key={id} src={urls.regular} alt={description}></img>
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
   });
 
   return(
-    <div>{images}</div>
+    <div className="image-list">{images}</div>
   )
 }
 
