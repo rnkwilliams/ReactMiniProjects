fce28cf0fba15fb568ef52a535e80b6657a01286
rnkwilliams <rnkwilliams07@gmail.com>
2021-02-08 11:19:30 -0600

    create app component and import to index.js

--- widgets-app/src/components/App.js
+++ widgets-app/src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Accordion from './Accordion'
 
 
 const App = () => { 
 
+  const items = [
+    {
+      title: 'What is React?',
+      content: 'React is a front end JavaScript framework.'
+    },
+    {
+      title: 'Why use React?',
+      content: 'React is a favorite JS library among engineers.'
+    },
+    {
+      title: 'How do you use React?',
+      content: 'You use React by creating components.'
+    }
+  ]
+
     return(
-      <div>App</div>
+      <div>
+        <Accordion items={items}/>
+      </div>
     )
 };
 
